4e9cd481a77df7ca0a4d985ebe85054a24971989
Barbara Appenzeller <appenzeller.barbara@gmail.com>
2021-04-28 16:59:46 +0100

    finish kata 7-4

--- katas/7-4-kata.js
+++ katas/7-4-kata.js
@@ -1,1 +1,18 @@
 // 7.4 kata: List filtering
+//In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+const test741 = [1, 2, 'a', 'b']; // [1,2]
+const test742 = [1, 'a', 'b', 0, 15]; // [1,0,15]
+const test743 = [1, 2, 'aasf', '1', '123', 123]; // [1,2,123]
+
+const test = (arr) => {
+  return newArr = arr.filter(x => typeof x === "number");
+}
+
+function filter_list(l) {
+  return l.filter(function (v) { return typeof v == 'number' })
+}
+
+console.log(test(test741));
+console.log(test(test742));
+console.log(test(test743));
