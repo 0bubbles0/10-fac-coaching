4132b129a3a0fb8e6ec4964077e95437061397aa
Barbara Appenzeller <appenzeller.barbara@gmail.com>
2021-04-29 18:43:14 +0100

    finish kata 6.3 + workshop

--- katas/6-3-kata.js
+++ katas/6-3-kata.js
@@ -12,7 +12,7 @@ const test622 = [1, 2, 2, 2, 3];
 function arrayDiff1(arr1, arr2) {
   const newArr = [];
   for (let i = 0; i < arr1.length; i++) {
-    if (arr2.indexOf(arr1[i] == -1)) {
+    if (arr2.indexOf(arr1[i])) {
       newArr.push(arr1[i]);
     }
   }
@@ -32,3 +32,43 @@ console.log(arrayDiff1([1, 2, 2, 2, 3], [2]));
 console.log();
 console.log();
 */
+
+
+
+//Michael
+/*kyu 7 – Coding Meetup #5 Higher -
+Take array of objects  return an object which includes the cound of each coding language represeented at the meetup
+*/
+
+function countLanguages(list) {
+  var output = [];
+  var C = list.filter(x => x.language === "C").length;
+  if (C > 0) {
+    output["C"] = C;
+  } else if (JS > 0) {
+    output["JavaScript"] = JS;
+  }
+  console.log(output;)
+}
+
+Danilo
+Challenge: four: (Thursday, 29 April):
+
+Write a function lookThroughObject;
+lookThroughObject accepts two arguments: an object("a"), and a function ("b");
+lookThroughObject must return an object that contains only those key - value pairs in "a" whose value causes "b" to return true
+
+Examples:
+lookThroughObjet({ name: 'Kim', age: 43, member: true }, (v) => typeof v === 'number')
+// Expected: { age: 43 }
+
+lookThroughObject({ firstName: 'Kim', lastName: 'Berly', address: '123 fake street' }, (v) => v.length > 3)
+// Expected: { lastName: 'Berly', address: '123 fake street' }
+
+lookThroughObject({ a: [], b: -1, c: [1, 2] }, (v) => Array.isArray(v) && v.length > 0)
+// Expected: { c: [1, 2] }
+
+This one is slightly more challenging, but do give it a shot!
+Here's a hint to help you get started, should you need it: Find out how to loop over objects in JavaScript
+Also, we'd much prefer if you ask for help when you get stuck instead of struggling alone, that's what #help - and - hints is for!
+
