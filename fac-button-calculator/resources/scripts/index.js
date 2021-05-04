d20b1824a7ad94cce62b9802b41f606ccaaf3566
Barbara Appenzeller <appenzeller.barbara@gmail.com>
2021-05-04 16:50:26 +0100

    fac button calculator try solve myself

--- fac-button-calculator/resources/scripts/index.js
+++ fac-button-calculator/resources/scripts/index.js
@@ -1,35 +1,34 @@
 //PART ONE
-
 //Get all numbers from the doc and add elements, handle values later. Try to make an array. 
 console.log(document.getElementsByClassName("number"));
-
 const numbers = document.getElementsByClassName("number");
 //const numArr = array.from(numbers); Wrong because syntax, like floor
 const numArr = Array.from(numbers);
-
 console.log(numArr);
 //Add event listener to every element. e is an object here, check Browser console
-
 function addActiveClass(element) {
   console.log(element.target);
   element.classList.toggle("active");
 }
-
 //Now, if we click on any box, the console shows working. Why does this work for all?
 /*for (let i = 0; i < numArr.length; i++) {
   numArr[i].addEventListener("click", e => {
     addActiveClass(e);
   });
 }*/
-
 const newNum = numArr.map(function (number) {
   number.addEventListener("click", event => {
-    addActiveClass(event.target)
-  });
+    if (newNum.length < 5 && newNum.indexOf(event.target) === -1) {
+      addActiveClass(event.target);
+    }
+    else {
+      newNum.shift();
+      addActiveClass(event.target);
+    }
+    return newNum;
+  })
 });
 console.log(numArr);
-
-
 //When a number is clicked, give it the CSS-class .active, And remove the class if the element has the class already --> see inside addActiveClass function-declaration
 
 //When a number is clicked, add the element to an array
