5b598475cc51e9edd9e17cbc2bf6f0185b32f5c1
Barbara Appenzeller <appenzeller.barbara@gmail.com>
2021-05-06 18:41:20 +0100

    k6 finish

--- katas/6-5-kata.js
+++ katas/6-5-kata.js
@@ -3,3 +3,44 @@ You live in the city of Cartesia where all roads are laid out in a perfect grid.
 Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
 */
 
+function isValidWalk(walk) {
+  let nPos = 0;
+  let wPos = 0;
+  for (let i = 0; i < walk.length; i++) {
+    switch (walk[i]) {
+      case 'n': nPos++; break;
+      case 's': nPos--; break;
+      case 'w': wPos++; break;
+      case 'e': wPos--; break;
+    }
+  }
+  if (walk.length === 10 && nPos === 0 && wPos === 0) {
+    return true;
+  }
+  return false;
+}
+
+//Other Solutions
+function isValidWalk(walk) {
+  var dx = 0
+  var dy = 0
+  var dt = walk.length
+
+  for (var i = 0; i < walk.length; i++) {
+    switch (walk[i]) {
+      case 'n': dy--; break
+      case 's': dy++; break
+      case 'w': dx--; break
+      case 'e': dx++; break
+    }
+  }
+
+  return dt === 10 && dx === 0 && dy === 0
+}
+
+function isValidWalk(walk) {
+  function count(val) {
+    return walk.filter(function (a) { return a == val; }).length;
+  }
+  return walk.length == 10 && count('n') == count('s') && count('w') == count('e');
+}
